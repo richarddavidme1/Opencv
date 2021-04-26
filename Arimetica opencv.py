c2c61291be71d5d1499fb09b3b6517bf55e955ee
richarddavidme1 <53422724+richarddavidme1@users.noreply.github.com>
2021-04-26 16:31:43 -0500

    Update Arimetica opencv.py

--- Arimetica opencv.py
+++ Arimetica opencv.py
@@ -1,10 +1,16 @@
+#Importacion de librerias
+#Import of libraries
 import cv2
 import numpy as np
+
+#Añadir direccion de la imagen importante ser las mismas dimensiones 
+# Add image address important to be the same dimensions
 
 img1=cv2.imread("D:\\Programacion\\Python\Opencv\\2.jpg")
 img2=cv2.imread("D:\\Programacion\\Python\Opencv\\1.png")
 
 #Sumar imagenes de forma ordenada
+#Add images in an orderly way
 
 sumar=cv2.add(img1,img2)
 peso = cv2.addWeighted(img1, 0.6, img2, 0.4, 0)
