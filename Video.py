aad98317fca25b691442f394839e3acc30080751
richarddavidme1 <53422724+richarddavidme1@users.noreply.github.com>
2021-05-09 16:44:03 -0500

    Update Video.py

--- Video.py
+++ Video.py
@@ -1,17 +1,24 @@
 import numpy as np
 import cv2
-import turtle
 
+#captura de video 0 para la camara predeterminada
 cap = cv2.VideoCapture(0)
 
 
 while (True):
+    
+    #Lectrua de fotogramas
     ret, frame1 = cap.read()
+    #Salida espejo
     frame = cv2.flip(frame1,1)
-    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #Transformacion a escala de grises
     gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    
+    #Salida de consola
     cv2.imshow('Pantalla Gris', gray)
     cv2.imshow("Pantalla Origianl",frame)
+    
+    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
